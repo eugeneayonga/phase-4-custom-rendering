86b45fa8de517c8242fea51df2ad260e17b2a1fb
Eugene <eugeneayonga@gmail.com>
2022-10-01 19:27:33 +0300

    commit tree 33b98b2caf79f35a7a63f9d56b044c203aa85395

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,43 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  # Removing Content When Rendering
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses, except: [:created_at, :updated_at]
+  # end
+
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #     id: cheese.id,
+  #     name: cheese.name,
+  #     price: cheese.price,
+  #     is_best_seller: cheese.is_best_seller
+  #   }
+  # end
+
+
+  #  Extending JSON Data with :methods
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese, only: [:id, :name, :price, :is_best_seller], methods: [:summary]
+  # end
+
+  # Basic Error Messaging When Rendering JSON Data
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: "Cheese not found" }, status: :not_found
+    end
   end
 
 end
